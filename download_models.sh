53580bc4fc45c79df9c0773a9215b81bee2fedb6
Nguyễn Minh Lý <82688630+lynguyenminh@users.noreply.github.com>
2023-03-15 22:42:04 +0700

    Create download_models.sh

---
+++ download_models.sh
@@ -0,0 +1,9 @@
+pip install -r gdown --upgrade
+pip install unzip
+
+
+cd vietnamese-scenetext-detection-recognition-api
+gdown --id 1Az9psFV6C1qiqFCL2s6DIBYRKr3Bkjd7
+unzip weights.zip
+rm -rf weights.zip
+cd ..
